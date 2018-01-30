d991cc9aa41451160a93cd5e2005eeaa34c0f9d8
austen barker <austenbarker@yahoo.com>
2018-01-29 20:17:10 -0800

    added script and rules

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # archi3configs
+
+Hotplugging displays:
+
+In order for hotplugging to work one must use a udev rule. This is contained in the file 98-monitor-hotplug.rules.
+
+To get a list of devices type the following.
+ls /sys/class/drm/card0
+
+To determine the status of the device run this:
+
+echo $(</sys/class/drm/card0/card0-<device>/status)
+
---
+++ monitorhotplug.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+#if something throws an error
+#set -e
+
+#There is a card0-DP-1 and DP-2, same is the case with the HDMI-A-1 and HDMI-A-2
+
+eDP_STATUS=$(</sys/class/drm/card0/card0-eDP-1/status )
+HDMI_STATUS=$(</sys/class/drm/card0/card-HDMI-A-1/status )
+DP_STATUS=$(</sys/class/drm/card0/card-DP-1/status )
+VGA_STATUS=$(</sys/class/drm/card0/card-VGA-1/status )
+
+eDP_STATUS=$(</sys/class/drm/card0/card0-eDP-1/enabled )
+HDMI_STATUS=$(</sys/class/drm/card0/card0-HDMI-A-1/enabled )
+DP_STATUS=$(</sys/class/drm/card0/card0-DP-1/enabled )
+VGA_STATUS=$(</sys/class/drm/card0/card0-VGA-1/enabled )
+
+#check for the state log
+if [ ! -f /tmp/monitor.log ]; then
+	touch /tmp/monitor
+	STATE=5
+else
+	STATE=$(</tmp/monitor)
+fi
+
+#the state log contains the next state to go in it
+
+#Monitors are disconnected, stay in state 1
+if [ "disconnected" == "$HDMI_STATUS" -a "disconnected" == "$DP_STATUS" -a "disconnected" == "$VGA_STATUS" ]; then
+	STATE=1
+fi
+
+case $STATE in
+	1)
+	#eDP is on, nothing connected
+	/usr/bin/xrandr --output eDP-1 --auto
+	STATE=2
+	;;
+	2)
+	#eDP is on, projectors connected but not on
+	/usr/bin/xrandr --output eDP-1 --auto --output HDMI-A-1 --off --output VGA-1 --off --output DP-1 --off
+      	STATE=3
+	;;
+	3)
+	#eDP is off, other things on
+	if [ "connected" == "$HDMI_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --off --output HDMI-A-1 --auto
+		TYPE="HDMI"
+	elif [ "connected" == "$DP_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --off --output DP-1 --auto
+		TYPE="DP"
+	elif [ "connected" == "$VGA_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --off --output VGA-1 --auto
+		TYPE="VGA"
+	fi
+	/usr/bin/notify-send -t 5000 --urgency=low "Switched to $TYPE"
+	STATE=4
+	;;
+	4)
+	#eDP is on, mirroring
+	if [ "connected" == "$HDMI_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --auto --output HDMI-A-1 --auto
+		TYPE="HDMI"
+	elif [ "connected" == "$DP_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --auto --output DP-1 --auto
+		TYPE="DP"
+	elif [ "connected" == "$VGA_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --auto --output VGA-1 --auto
+		TYPE="VGA"
+	fi
+	/usr/bin/notify-send -t 5000 --urgency=low "switched to $TYPE mirroring"
+	STATE=5
+	;;
+	5)
+	#eDP is on, extending
+	if [ "connected" == "$HDMI_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --auto --output HDMI-A-1 --auto --left-of eDP-1
+		TYPE="HDMI"
+	elif [ "connected" == "$DP_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --auto --output DP-1 --auto --left-of eDP-1
+		TYPE="DP"
+	elif [ "connected" == "$VGA_STATUS" ]; then
+		/usr/bin/xrandr --output eDP-1 --auto --output VGA-1 --auto --left-of eDP-1
+		TYPE="VGA"
+	fi
+	/usr/bin/notify-send -t 5000 --urgency=low "switched to $TYPE extended"
+	STATE=2
+	;;
+	*)
+	#this is strange, assume 1
+	STATE=1
+	;;
+esac
+
+echo $STATE > /tmp/monitor
